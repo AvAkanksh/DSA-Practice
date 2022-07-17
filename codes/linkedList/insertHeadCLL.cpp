5fc656231aa2f65f1193bea31428154a42d604e8
AvAkanksh <ee18b002@iittp.ac.in>
2022-07-17 18:33:50 +0530

    updating the files

---
+++ codes/linkedList/insertHeadCLL.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node{
+    int data ;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void traverseCLL(Node *head){
+
+    Node *curr = head;
+    do{
+        cout<<curr->data<<"\t";
+        curr = curr->next;
+    }while(curr!=head);
+    cout<<"\n";
+}
+
+Node *insertHeadCLL(Node *head, int value){
+    Node *temp = new Node(value);
+    if(head == NULL){
+        temp->next = temp;
+        return temp;
+    }
+    else{
+        temp->next = head->next;
+        head->next = temp;
+        temp->data = head->data;
+        head->data = value;
+        return head;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = head;
+
+    traverseCLL(head);
+    head = insertHeadCLL(head,99);
+    traverseCLL(head);
+
+
+    return 0;
+}
